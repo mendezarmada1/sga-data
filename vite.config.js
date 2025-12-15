68bde487605320358992ade403ca9cb0a2505a99
mendezarmada1 <mendezarmadasebastianjose@gmail.com>
2025-12-15 18:12:11 +0100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SGA/", // Reemplaza 'SGA' con el nombre de tu repositorio si es diferente
+  base: "/sga-data/", // Reemplaza 'SGA' con el nombre de tu repositorio si es diferente
 })
